fbf4781a6b6d91e82eb8d8ee662b645651b30665
greeng00se <thegreengoose7@gmail.com>
2023-04-01 07:08:31 +0000

    deploy: 72a1430ef26b3c36eeff24cba52004ef37d4d05d

--- assets/js/0c071de2.aa656974.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[321],{3125:e=>{e.exports=JSON.parse('{"permalink":"/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":12,"previousPage":"/","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/a5557bb9.11b22a5a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5991],{3885:e=>{e.exports=JSON.parse('{"permalink":"/","page":1,"postsPerPage":10,"totalPages":2,"totalCount":12,"nextPage":"/page/2","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
