694ca42bd2ce25ff2cd6c2f2a4fdb527cfb4cbb0
greeng00se <thegreengoose7@gmail.com>
2023-04-02 15:23:31 +0000

    deploy: ae5fe3a516bb62563035792e76d472f799e0ac2e

---
+++ assets/js/33736670.5fac410b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2742],{700:s=>{s.exports=JSON.parse('{"label":"Class","permalink":"/tags/class","allTagsPath":"/tags","count":1}')}}]);
