537dcbf9247bcf3e456b7cd333643658b0778f1b
greeng00se <thegreengoose7@gmail.com>
2023-05-25 12:56:58 +0000

    deploy: b50e81bb83f4d62e6b590478677353ade6642bfc

---
+++ assets/js/5a6c6934.e9ceaffa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5953],{8630:e=>{e.exports=JSON.parse('{"label":"DTO","permalink":"/tags/dto","allTagsPath":"/tags","count":1}')}}]);
---
+++ assets/js/d0277431.0c6e8615.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[846],{4838:e=>{e.exports=JSON.parse('{"permalink":"/tags/dto","page":1,"postsPerPage":1,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
